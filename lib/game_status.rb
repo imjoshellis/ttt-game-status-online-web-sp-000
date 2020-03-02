82465dc31a1fab3ab052eb77e9c9010e093316cc
Josh Ellis <joshe523@gmail.com>
2020-03-02 15:46:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,9 +13,10 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
     if board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]]
-      if !(board[combo[0]] == " " || board[combo[0]] == "")
+      if board[combo[0]] == " " && board[combo[0]] == ""
         return combo
       end
     end
   end
+  return nil
 end
